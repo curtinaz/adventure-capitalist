b267e922baa08dc5396bebdadb7333aafd1a2c1b
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-01-17 21:24:12 -0300

    Update game.js

--- game.js
+++ game.js
@@ -249,13 +249,14 @@ function footerMenu() {
   footerBtn.classList.add('is-active')
   footerBtn.setAttribute('onClick', 'footerMenuOff()')
   // console.log("Open");
-  
+
 }
 
 function footerMenuOff() {
   footerBtn.classList.remove('is-active')
   footerBtn.setAttribute('onClick', 'footerMenu()')
   // console.log("Close");
+  
 }
 
 
